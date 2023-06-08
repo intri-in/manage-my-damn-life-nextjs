168156d5cf6014c470d412199797c0ede9967261
anshuk <personal_e@anshukattri.in>
2023-06-08 21:48:45 +0530

    - Fixed bug: task list in Home Page not being updated on adding a new task - Bit of rewrite to home page -- getting ready for user to customise home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,125 +6,43 @@ import { getTodaysDateUnixTimeStamp, varNotEmpty } from '@/helpers/general'
 import DashboardView from '@/components/fullcalendar/DashboardView'
 import AddTask from '@/components/common/AddTask/AddTask'
 import { PRIMARY_COLOUR, SECONDARY_COLOUR } from '@/config/style'
-import { Component } from 'react'
+import { Component, useEffect, useState } from 'react'
 import { getI18nObject } from '@/helpers/frontend/general'
 import { MYDAY_LABEL } from '@/config/constants'
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { AiOutlineMenuUnfold } from 'react-icons/ai'
 import Script from 'next/script'
 import HomeTasks from '@/components/Home/HomeTasks/HomeTasks'
+import CombinedView from '@/components/page/CombinedView'
+import { Loading } from '@/components/common/Loading'
 
-export default class Home extends Component {
-  constructor(props)
-  {
-    super(props)
-    this.i18next = getI18nObject()
-    this.state = {updated: "", scheduleItem: null ,showListColumn: true, calendarAR: 1.35}
-    this.fetchEvents = this.fetchEvents.bind(this)
-    this.scheduleItem = this.scheduleItem.bind(this)
-    this.updateDimensions = this.updateDimensions.bind(this)
-    this.showListColumnButtonClicked = this.showListColumnButtonClicked.bind(this)
-    this.handleCloseOffcanvas = this.handleCloseOffcanvas.bind(this)
-  }
+const i18next = getI18nObject()
 
-  componentDidMount(){
-    if (window != undefined) {
-     // window.addEventListener('resize', this.updateDimensions);
+export default function HomePage() {
+  const [updated, setUpdated]=useState('')
+  const [isSyncing, setIsSyncing] = useState(false)
 
-      if (window.innerWidth < 900) {
-        this.setState({ showListColumn: false })
-
-      } else {
-        this.setState({ showListColumn: true })
-
-      }
+  const onSynComplete = () =>{
+      var updated = Math.floor(Date.now() / 1000)
+      setUpdated(updated)
     }
 
+  const [finalOutput, setFinalOutput] =useState(<CombinedView updated={updated} onSynComplete={onSynComplete} />)
 
-  }
-
-  updateDimensions = () => {
-    if (window.innerWidth < 700) {
-      
-      this.setState({ showListColumn: false, calendarAR: 0.3 })
-
-    } else {
-      this.setState({ showListColumn: true })
-
-    }
-  };
-
-  showListColumnButtonClicked() {
-    this.setState({ showLeftColumnOffcanvas: true, })
-  }
-  handleCloseOffcanvas() {
-    this.setState({ showLeftColumnOffcanvas: false })
-  }
-
-
-
-  fetchEvents() {
-    var updated = Math.floor(Date.now() / 1000)
-    this.setState({updated: updated})
-  }
-
-  scheduleItem(data)
-  {
-    console.log(data)
-    if(varNotEmpty(data) && varNotEmpty(data.summary) && varNotEmpty(data.calendar_id))
-    {
-      this.setState({scheduleItem : data})
-    }
-  }
-  render(){
-    var borderLeft = this.state.showListColumn ? '3px solid ' + SECONDARY_COLOUR : ""
-    var leftColumnMatter = ( <><AddTask  />
-    <HomeTasks scheduleItem={this.scheduleItem} updated={this.state.updated} fetchEvents={this.fetchEvents} view="tasklist" caldav_accounts_id={null} calendars_id={null} filter={{}} />
-    
-{/*       
-    <h3 style={{marginTop: 30}}>My Day</h3>
-<TaskList scheduleItem={this.scheduleItem} updated={this.state.updated} fetchEvents={this.fetchEvents} view="tasklist" caldav_accounts_id={null} calendars_id={null} filter={{logic: "or", filter: {due:[0, getTodaysDateUnixTimeStamp()], label: [MYDAY_LABEL]}}} />
- */}      </>)
-      var listColumn = !this.state.showListColumn ? null : (
-        <Col lg={4} style={{ paddingTop: 30 }} >
-          {leftColumnMatter}
-        </Col>
-      )
-      var expandColButton = this.state.showListColumn ? null : <div style={{ marginTop: 20 }}><AiOutlineMenuUnfold color={PRIMARY_COLOUR} onClick={this.showListColumnButtonClicked} size={24} /></div>
-
-    return (
-      <>
+    useEffect(()=>{
+    })
+    return(
+        <>
         <Head>
-          <title>{this.i18next.t("APP_NAME_TITLE")} - {this.i18next.t("TASKS")}</title>
+          <title>{i18next.t("APP_NAME_TITLE")} - {i18next.t("TASKS")}</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
         </Head>
-        <AppBarGeneric  onSynComplete={this.onSynComplete} />
+        <AppBarGeneric  onSynComplete={onSynComplete} isSyncing={isSyncing}/>
         <div className='container-fluid'>
-  
-          <Row style={{}}>
-            {listColumn}
-            <Col lg={8} style={{paddingTop: 20, borderLeft: borderLeft}}>
-            {expandColButton}
+            {finalOutput}
+        </div>     
+        </>
 
-            <DashboardView calendarAR={this.state.calendarAR} scheduleItem={this.state.scheduleItem} />
-            </Col>
-          </Row> 
-          <Offcanvas placement='start' show={this.state.showLeftColumnOffcanvas} onHide={this.handleCloseOffcanvas}>
-          <Offcanvas.Header closeButton>
-            <Offcanvas.Title></Offcanvas.Title>
-          </Offcanvas.Header>
-          <Offcanvas.Body>
-            {leftColumnMatter}
-          </Offcanvas.Body>
-        </Offcanvas>
-
-  
-        </div>
-                  
-      </>
     )
-    }
-
-
 }
