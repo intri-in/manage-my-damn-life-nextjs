165c9563a691b1f3857bd6e2f1f99a21059cc866
Tobin Rosenau <tobin.rosenau@mailbox.org>
2025-04-12 14:25:03 +0200

    Revert "Merge branch 'add-db_port-support-for-sequelize' into local-test"
    This reverts commit c31a50ff21c183155c95d68f2feeb7a20de2c693, reversing
    changes made to db6e012924a7f19c20311cdba3a5588e7d9f10a2.

--- src/helpers/api/db.js
+++ src/helpers/api/db.js
@@ -43,7 +43,6 @@ export function getSequelizeObj(){
     process.env.DB_PASS,
      {
        host: process.env.DB_HOST,
-       port: process.env.DB_PORT,
        dialect: 'mysql',
        dialectModule: require('mysql2'),
      });
