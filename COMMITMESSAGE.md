246e28265e03ea1dfe176e5e00990086a66c9520
anshuk <dev@anshukattri.in>
2025-12-23 20:09:54 +0530

    Fixed #271     - Bug was caused because Jotai was only getting a static value. It now fetches value from setting. Other Bug Fixes     - Username is now extracted correctly. It was being taken from session data, regardless of whether NextAuth was enabled or not.

--- COMMITMESSAGE.md
+++ COMMITMESSAGE.md
@@ -1,5 +1,4 @@
-Fixed #283
-    - User now received unhashed OTP.
-    - version bump to 0.8.3-dev    
-    - Removed console log of unhashed otp
-
+Fixed #271
+    - Bug was caused because Jotai was only getting a static value. It now fetches value from setting.
+Other Bug Fixes
+    - Username is now extracted correctly. It was being taken from session data, regardless of whether NextAuth was enabled or not.
--- stateStore/SettingsStore.ts
+++ stateStore/SettingsStore.ts
@@ -1,8 +1,8 @@
-import { SETTING_NAME_DATE_FORMAT, SETTING_NAME_TIME_FORMAT } from '@/helpers/frontend/settings'
+import { getUserSetDateFormat, getUserSetTimeFormat, SETTING_NAME_DATE_FORMAT, SETTING_NAME_TIME_FORMAT } from '@/helpers/frontend/settings'
 import { atom } from 'jotai'
 
 
-export const currentSimpleDateFormatAtom = atom("DD/MM/YYYY")
-export const currentSimpleTimeFormatAtom = atom("HH:mm")
+export const currentSimpleDateFormatAtom = (typeof(window)=="undefined")? atom("DD/MM/YYYY"):atom(getUserSetDateFormat())
+export const currentSimpleTimeFormatAtom = (typeof(window)=="undefined")? atom("HH:mm"): atom(getUserSetTimeFormat())
 
 export const currentDateFormatAtom = atom((get) => get(currentSimpleDateFormatAtom)+" "+get(currentSimpleTimeFormatAtom))
