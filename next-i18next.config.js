c39785214709d9474e92fd74adbc9c6d5e25a391
anshuk <dev@anshukattri.in>
2026-07-19 16:34:26 +0530

    yet another workflow error fix

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,11 +1,14 @@
-const { AVAILABLE_LANGUAGES } = require('./src/config/constants');
 
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
     i18n: {
     defaultLocale: 'en',
     
-    locales: AVAILABLE_LANGUAGES,
+    locales: [
+    "en",
+    "de",
+    "fr"
+    ],
     },
     
 }
