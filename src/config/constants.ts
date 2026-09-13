846a63ce691228d20761b981ec523859a88b7860
anshuk <dev@anshukattri.in>
2026-09-13 11:07:17 +0530

    Version bump to v0.10.0 before launch.

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next'
-export const VERSION_NUMBER = "0.10.0-dev-rev1" 
+export const VERSION_NUMBER = "0.10.0" 
 /*
 * SYSTEM_DEFAULT_LABEL_PREFIX: Default prefix applied to all system generated labels like
 * "My Day"
